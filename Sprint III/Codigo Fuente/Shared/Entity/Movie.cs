fda9154e3c7e77cb00f6a8fb5f228dac7c985eff
nerges7 <87239449+nerges7@users.noreply.github.com>
2021-09-26 20:56:54 -0500

    Add files via upload

---
+++ Sprint III/Codigo Fuente/Shared/Entity/Movie.cs
@@ -0,0 +1,13 @@
+using System;
+namespace popcorntime.Shared.Entity
+{
+    public class Movie
+    {
+        public string MovieName{get;set;}
+        public string Image{get;set;}
+        public string Score{get;set;}
+        public string Category{get;set;}
+        public string Synopsis{get;set;}
+        public DateTime Premier {get;set;}
+    }
+}
